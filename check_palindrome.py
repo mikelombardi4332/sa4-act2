cd167224a0a48b78ab87adfb150d3ed66c14625e
Michael Lombardi <mikelombardi4332@gmail.com>
2024-02-07 12:59:43 -0600

    Replaced hardcoded string with user-input text string

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 def check_palindrome(text):
     if text == text[::-1]:
         print(f"{text} is a palindrome")
     else:
         print(f"{text} is not a palindrome")
-        
-        
-text = "did"
-check_palindrome(text) 
+    
+    
+    
+text = input("Enter text to check for palindrome: ")
+check_palindrome(text)
